6776fb2a1c9607758e4cac017911c80b456d3971
Riley Leong <117493932+rtleong@users.noreply.github.com>
2023-01-11 23:20:47 -0800

    Add files via upload

---
+++ Project1/Player.h
@@ -0,0 +1,32 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Arena.h"
+
+class Arena;
+//Player declaration
+class Player
+{
+public:
+    // Constructor
+    Player(Arena* ap, int r, int c);
+
+    // Accessors
+    int  row() const;
+    int  col() const;
+    bool isDead() const;
+
+    // Mutators
+    string dropPoisonedCarrot();
+    string move(int dir);
+    void   setDead();
+
+private:
+    Arena* m_arena;
+    int    m_row;
+    int    m_col;
+    bool   m_dead;
+};
+
+
+#endif
---
+++ Project1/main.cpp
@@ -0,0 +1,19 @@
+#include "Arena.h"
+#include "globals.h"
+#include "Player.h"
+#include "Rabbit.h"
+#include "Game.h"
+
+///////////////////////////////////////////////////////////////////////////
+// main()
+///////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    // Create a game
+    // Use this instead to create a mini-game:   Game g(3, 5, 2);
+    Game g(10, 12, 40);
+
+    // Play the game
+    g.play();
+}
